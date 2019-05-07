e75951be70cdb82b83cd1a8265b4fd39777daaa6
n0200688 <n0200688@LIBP35P-UH3DC5M.lm.lmig.com>
2019-05-07 16:23:16 -0400

    Commit to github private

---
+++ ObjectDetectionJavaApp/src/main/java/com/pktam01/objectdetection/main/StorageProperties.java
@@ -0,0 +1,21 @@
+package com.pktam01.objectdetection.main;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+    /**
+     * Folder location for storing files
+     */
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
